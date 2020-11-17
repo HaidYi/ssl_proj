33dab75023a48aa34c3d5b37286b1216ecd6d14e
zjgbz <minky.k.jiang@gmail.com>
2020-11-17 14:43:20 -0500

    modified:   scripts/run_cifar10.sh

--- scripts/run_cifar10.sh
+++ scripts/run_cifar10.sh
@@ -3,6 +3,7 @@
 GPU_ID=$1
 batch_size=$2
 augtype=$3
+n_epochs=$4
 
 CUDA_VISIBLE_DEVICES=${GPU_ID} python ../train.py \
   --dset cifar10 \
@@ -11,5 +12,5 @@ CUDA_VISIBLE_DEVICES=${GPU_ID} python ../train.py \
   --batch_size ${batch_size} \
   --log_dir "../experiment/batch_size_${batch_size}" \
   --augtype ${augtype} \
-  --n_epochs 2 \
+  --n_epochs ${n_epochs} \
   --use_cuda
